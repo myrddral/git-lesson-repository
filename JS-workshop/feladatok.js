da3e2bf6832eb7864531a2456aeb3ee429a15643
myrddral <thirdeye0@gmail.com>
2020-12-15 10:58:53 +0100

    array workshop tasks added

---
+++ JS-workshop/feladatok.js
@@ -0,0 +1,110 @@
+// Third
+'use strict';
+// Hozz létre egy `numbers` változót az alábbi tartalommal:
+// `[1, 3, 5, 7]`
+// Logold ki a `numbers` harmadik elemét a konzolra!
+
+//Compare Length
+'use strict';
+// Hozz létre egy `firstArrayOfNumbers` változót az alábbi tartalommal:
+// `[1, 2, 3]`
+// Hozz létre egy `secondArrayOfNumbers` változót az alábbi tartalommal:
+// `[4, 5]`
+// Logold ki a konzolra, hogy 'secondArrayOfNumbers hosszabb, mint a
+// firstArrayOfNumbers' hogyha a 'secondArrayOfNumbers'-ben több elem van, mint
+// a 'firstArrayOfNumbers'-ben
+
+//Sum elements
+'use strict';
+// Hozz létre egy `numbers` változót az alábbi tartalommal:
+// `[54, 23, 66, 12]`
+// Logold ki a második és harmadik elem összegét a konzolra!
+
+//Swap elements
+'use strict';
+// Hozz létre egy `names` változót az alábbi tartalommal:
+// `["Arthur", "Boe", "Chloe"]`
+// Cseréld fel a names első és harmadik elemét!
+// Logold ki az eredményt a konzolra!
+
+//Print elements
+'use strict';
+// Hozz létre egy `numbers` változót az alábbi tartalommal: `[4, 5, 6, 7]`
+// Logold ki a konzolra a numbers tömb mindegyik elemét
+// *hint: használj ciklust, a console.log(numbers) nem jó megoldás!
+// Bónuszpont, hogyha ezt megcsinálod a megfelelő beépített függvénnyel is
+
+//Change element
+'use strict';
+// Hozz létre egy `numbers` változót az alábbi tartalommal: `[1, 2, 3, 8, 5, 6]`
+// Változtasd meg a 8-as számot 4-essé a map metódussal
+// Logold ki a 4. elemet
+
+//Increment element
+'use strict';
+// Hozz létre egy `numbers` változót az alábbi tartalommal:
+// `[1, 2, 3, 4, 5]`
+// Növeld meg a harmadik elem értékét!
+// Logold ki a harmadik elemet a konzolra!
+
+//Append a
+'use strict';
+// Hozz létre egy `animals` változót az alábbi tartalommal:
+// `['koal', 'pand', 'zebr']`
+// Minden elemhez fűzz hozzá egy "a" betűt
+// Próbálj meg beépített metódust használni ciklus helyett!
+
+//Double items
+'use strict';
+// Hozz létre egy `drinks` változót az alábbi tartalommal:
+// `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// Duplázd meg a stringeket (szöveges elemeket)!
+// Használj beépített metódust a ciklusok helyett!
+// Logold ki az eredményt!
+// A várt eredmény: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+//Colors
+'use strict';
+// Hozz létre egy kétdimenziós tömböt, ami különböző színek egyes árnyalatait
+// tartalmazza.
+// A `colors[0]` a zöld árnyalatait tartalmazza:
+// `"lime", "forest green", "olive", "pale green", "spring green"`
+// A `colors[1]` a piros árnyalatait tartalmazza:
+//   `"orange red", "red", "tomato"`
+// A `colors[2]` a rózsaszín árnyalatait tartalmazza:
+//   `"orchid", "violet", "pink", "hot pink"`
+
+let colors = [];
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+//Sum all elements
+'use strict';
+// Hozz létre egy `numbers` változót az alábbi tartalommal: `[3, 4, 5, 6, 7]`
+// Logold ki az elemek összegét a konzolra!
+
+//Matrix
+'use strict';
+// Hozz létre dinamikusan* egy kétdimenziós tömböt!
+// Tartalma az alábbi mátrix** legyen.
+// Használj ciklust hozzá!
+
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+
+// Logold ki a mátrixot a konzolra.
+//
+// * a mérete egy változóból érkezzen, így téve dinamikussá a mátrix létrehozását
+// ** ne ijedj meg a kifejezéstől, egy mátrix: tömbök a tömbben
+
+//Reverse list
+'use strict';
+// Hozz létre egy `numbers` változót az alábbi tartalommal: `[3, 4, 5, 6, 7]`
+// Fordítsd meg a bejárás sorrendjét, kezdd a végéről!
+// - beéptett metódussal
+// - egy átmeneti arrayt létrehozva, és egy ciklus segítségével
+// Logold ki a konzolra a megfordított numbers tömb mindegyik elemét
